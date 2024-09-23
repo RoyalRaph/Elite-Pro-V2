1b1f3527f82b3b127ddbe3582a7337d59bcb866c
RoyalRaph <raphaelaugustin89@gmail.com>
2024-09-23 10:15:46 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,24 +6,24 @@ const chalk = require('chalk')
 global.sessionid ='put your session id here'
 
 //owmner v card
-global.ytname = "YT: ELITE-PRO-V2" //ur yt chanel name
-global.socialm = "GitHub: Elite-MD" //ur github or insta name
+global.ytname = "YT: Royal Raph-MD" //ur yt chanel name
+global.socialm = "GitHub: Royal Raph-MD" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = 'ROYAL RAPH-V2' //ur bot name
+global.ownernumber = '23057130371' //ur owner number
+global.ownername = 'ROYAL RAPH-PRO-V2' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
 global.themeemoji = '👨🏼‍💻'
-global.wm = "ELITE-PRO-V2"
-global.botscript = 'https://github.com/Elite-MD/ELITE-PRO-V2' //script link
+global.wm = "ROYAL RAPH-PRO-V2"
+global.botscript = 'https://github.com/Royal Raph-MD/ROYAL RAPH-PRO-V2' //script link
 global.packname = "Sticker By"
-global.author = "ELITE-PRO-V2"
-global.creator = "2348109263390@s.whatsapp.net"
+global.author = "ROYAL RAPH-PRO-V2"
+global.creator = "23057130371@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["2348109263390"] // Premium User
+global.premium = ["23057130371"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -33,7 +33,7 @@ global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
